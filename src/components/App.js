ea7a68ecb2d07c78720395619dcbbd6a6eeb1361
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-22 13:54:35 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
+  const[expenseInput, setExpenseInput] = useState('');
+  const[expenseList, setExpenseList] = useState([]);
+  const[totalExpense, setTotalExpense] = useState(0);
+
+  const handleExpenceButton = () => {
+    const[itemName, itemPrice] = expenseInput.split("-").map(i => i.trim());
+    setExpenseList([...expenseList, {itemName, itemPrice}]);
+    setTotalExpense(totalExpense + parseFloat(itemPrice));
+  }
 
   return (
     <div id="main">
-      <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <input id="expense-input" value={expenseInput} onChange={e => setExpenseInput(e.target.value)}/>
+      <button id="expense-button" onClick={handleExpenceButton}>Add Expense</button>
       <div id="expense-list">
+        {expenseList.map((expense, index) => {
+          <div key={index}>{expense.itemName} - {expense.itemPrice}</div>
+        })}
       </div>
       <div id="total-expense">
-        Total Expense: 0
+        Total Expense: {totalExpense}
       </div>
     </div>
   )
